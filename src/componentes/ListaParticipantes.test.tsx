dd82550101c5ba6489e7b671fdf427d96e516c82
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-24 11:00:51 +0100

    Att Lista

--- src/componentes/ListaParticipantes.test.tsx
+++ src/componentes/ListaParticipantes.test.tsx
@@ -4,10 +4,12 @@ import { render,screen } from "@testing-library/react"
 import ListaParticipantes from "./ListaParticipantes"
 
 describe('Uma lista vazia de participantes',()=>{
+  test('Deve ser renderizada sem elementos',()=>{
     render(<RecoilRoot>
         <ListaParticipantes/>
     </RecoilRoot>)
 
     const itens = screen.queryAllByRole('listitem')
     expect(itens).toHaveLength(0)
+  })
 })
