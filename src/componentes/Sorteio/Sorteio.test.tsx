4ab8ec84a4c48b039d92cb5d5012902304baccea
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-24 13:37:00 +0100

    Paginas

---
+++ src/componentes/Sorteio/Sorteio.test.tsx
@@ -0,0 +1,65 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { useListaDeParticipantes } from "../../state/hooks/useListaDeParticipantes"
+import { useResultadoSorteio } from "../../state/hooks/useResultadoSorteio"
+import Sorteio from "./Sorteio";
+
+jest.mock('../state/hook/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+})
+jest.mock('../state/hook/useResultadoSorteio', () => {
+    return {
+        useResultadoSorteio: jest.fn()
+    }
+})
+
+describe('na pagina de sorteio', () => {
+    const participantes = [
+        'Maria',
+        'Vanessa',
+        'Andressa'
+    ]
+    const resultado = new Map([
+        ['Vanessa', 'Maria'],
+        ['Maria', 'Andressa'],
+        ['Andressa', 'Vanessa']
+    ])
+
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+        (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
+    })
+    test('todos os participantes podem exibir o seu amigo secreto', () => {
+        render(<RecoilRoot>
+            <Sorteio />
+        </RecoilRoot>)
+
+        const opcoes = screen.queryAllByRole('option')
+        expect(opcoes).toHaveLength(participantes.length)
+    })
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(<RecoilRoot>
+            <Sorteio />
+        </RecoilRoot>)
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+        
+        fireEvent.change(select, {
+            target: {
+                value: participantes[0]
+            }
+        })
+
+        const botao = screen.getByRole('button')
+
+        fireEvent.click(botao)
+
+        const amigoSecreto = screen.getByRole('alert')
+
+        expect(amigoSecreto).toBeInTheDocument()
+
+    })
+})
--- src/state/Atom.ts
+++ src/state/Atom.ts
@@ -4,6 +4,13 @@ export const listaDeParticipantesState = atom<string[]>({
     key:'listaDeParticipantesState',
     default:[]
 })
+
+export const resultadoAmigoSecreto = atom<Map<string, string>>({
+    key: 'resultadoAmigoSecreto',
+    default: new Map()
+})
+
+
 export const erroState = atom<string>({
     key:'erroState',
     default:''
